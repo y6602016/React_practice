457857e8852c5cb482d0a0b4d95ee597c057a5dc
mike <y6602016@gmail.com>
2021-06-20 21:34:02 +0800

    add section 27

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 
-import App from "./section_18/components/App";
+import App from "./section_27/components/App";
 import reducers from "./section_18/reducers";
 
 const store = createStore(reducers, applyMiddleware(thunk));
